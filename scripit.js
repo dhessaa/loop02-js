88c1bfe619c512472b45ccea9f63c6f42b9a4990
dhessaa <andressasouza017@gmail.com>
2025-04-08 21:18:19 -0300

    Add files via upload

---
+++ scripit.js
@@ -0,0 +1,17 @@
+var carro = {
+    marca: "ford",
+    modelo: "fusion",
+    ano: 2020,
+    portas: 4,
+    cor: "branco",
+    preco: 126750 ,
+}
+
+alert(`marca: ${carro.marca} modelo: ${carro.modelo}
+ ano: ${carro.ano} portas: ${carro.portas} 
+ cor: ${carro.cor} preco: ${carro.preco}`)
+
+
+
+
+
